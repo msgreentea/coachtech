431681d937cbebfb4faadde9b63ae9c3162d4c7c
msgreentea <72728561+msgreentea@users.noreply.github.com>
2021-10-01 17:07:40 +0900

    Add files via upload

---
+++ coachtech/css/UI/modal.js
@@ -0,0 +1,15 @@
+const openBtn = document.getElementById('openBtn');
+const closeBtn = document.getElementById('closeBtn');
+const modal = dodument.getElementById('modal');
+
+openBtn.addEventListener('click', () => {
+  modal.style.display = 'block';
+})
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+})
+window.addEventListener('click', (e) => {
+  if (!e.target.closest('.modal__content-inner') && e.target.id !== "openBtn") {
+    modal.style.display = 'none';
+  }
+})
